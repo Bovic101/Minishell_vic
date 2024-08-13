98e06d83d6ea3316701f16f6835113ee95c2fe0d
Katsiaryna Dvarakouskaya <kdvarako@2-G-4.42heilbronn.de>
2024-08-13 17:29:36 +0200

    added libft

---
+++ libft/ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/18 12:13:28 by kdvarako          #+#    #+#             */
+/*   Updated: 2024/08/13 14:39:53 by kdvarako         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *str)
+{
+	int		i;
+	int		len;
+	char	*ptr;
+
+	i = 0;
+	if (str == NULL)
+		return (NULL);
+	len = ft_strlen(str);
+	ptr = (char *)malloc(len + 1);
+	if (!ptr)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
