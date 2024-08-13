a9d4ea2cf0abf5f1392e8591259e63a07d0043ab
Katsiaryna Dvarakouskaya <kdvarako@2-G-4.42heilbronn.de>
2024-08-13 17:22:06 +0200

    minishell.h

---
+++ minishell.h
@@ -0,0 +1,142 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/02 12:18:18 by kdvarako          #+#    #+#             */
+/*   Updated: 2024/08/12 17:11:01 by kdvarako         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+enum e_tokentype
+{
+	WORD,
+	SEPARATOR,
+	REDIR_IN,
+	REDIR_OUT,
+	REDIR_IN2,
+	REDIR_OUT2,
+	EXPAND_VAR,
+	EXPAND_VAR_Q,
+	PIPE
+};
+
+typedef struct s_token
+{
+	int					len;
+	char				*ptr;
+	enum e_tokentype	type;
+	struct s_token		*next;
+}		t_token;
+
+//structure for arguments: array of char
+
+typedef struct s_arg
+{
+	char			*value;
+	struct s_arg	*next;
+}	t_arg;
+
+//structure for redirection: type(<,<<,>,>>) - name_of_file
+
+typedef struct s_redirect
+{
+	char				*rtype;
+	char				*rfile;
+	struct s_redirect	*next;
+}	t_redirect;
+
+/*
+structure to execute: 
+command - array_struct_of_arguments - array_struct_of_redirection
+*/
+
+typedef struct s_parc
+{
+	char				*cmd;
+	struct s_arg		*args;
+	struct s_redirect	*redirs;
+	struct s_parc		*next;
+}	t_parc;
+
+/*
+structure to save env: key=value
+$? - status of the most recently executed pipeline
+-> value of the variable "exit_code" in env
+"exit_code" is a nonprintable variable
+*/
+
+typedef struct s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}	t_env;
+
+/*
+structure to save a history of commands:
+rline - a current readline
+next - next cmd
+prev - privios cmd
+*/
+
+typedef struct s_history
+{
+	char				*rvalue;
+	struct s_history	*next;
+	struct s_history	*prev;
+}	t_history;
+
+void		lexer(t_token **token, char *s);
+t_token		*ft_lst_new(int len, char *ptr, enum e_tokentype type);
+void		ft_lst_add_back(t_token **token, t_token *new);
+void		ft_lst_print(t_token **token);
+void		freeall(t_token **token, t_parc **parc);
+void		parcer(t_token **token, t_parc **parc, t_env **env);
+t_parc		*ft_plst_new(char *cmd, t_arg *args, t_redirect *redirs);
+void		ft_plst_add_back(t_parc **parc, t_parc *new);
+void		ft_plst_print(t_parc **parc);
+void		add_redir(char *rtype, char	*rfile, t_redirect **redirs);
+void		ft_redir_print(t_redirect **redirs);
+void		add_args(char *value, t_arg **args);
+void		ft_arg_print(t_arg **args);
+char		*ft_strndup(const char *str, size_t n);
+void		save_environment(char **envp, t_env **env);
+t_env		*ft_env_new(char *key, char *value);
+void		ft_env_add_back(t_env **lst, t_env *new);
+//to print env
+void		ft_env_print(t_env **env);
+//to sort and print env with declare
+void		ft_env_sort_declare(t_env *env);
+void		add_env(char *key, char *value, t_env **env);
+int			ft_strcmp(char *s1, char *s2);
+void		ft_free_env(t_env	**env);
+//return value of the key-variable from env
+char		*get_value(char *key, t_env **env);
+//set a new value to env (new_value must be allocated)
+void		set_value(char *key, char *new_value, t_env **env);
+//remove info from env
+void		remove_node(char *key, t_env **env);
+char		*expand(char *ptr, int len, t_env **env, int type);
+//funtions for the history of commands:
+void		save_history(char *s, t_history **history);
+t_history	*ft_history_new(char *rvalue);
+void		ft_history_add_back(t_history **lst, t_history *new);
+void		ft_free_history(t_history	**history);
+void		ft_history_print(t_history **history);
+//execution part:
+void		execute(t_parc **parc, t_env **env);
+
+#endif
