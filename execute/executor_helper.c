1b52e4b3d3fa0a0b0ef1f984cddafec4f971b275
vodebunm <vodebunm@student.42heilbronn.de>
2024-08-27 13:49:44 +0200

    Basic formatting of source code

--- execute/executor_helper.c
+++ execute/executor_helper.c
@@ -6,31 +6,33 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 13:20:14 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/08/26 12:53:00 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/08/27 13:47:39 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
 /*
 execve() function requires array to execute a commannd
 **/
-//convert linked_lst of arg(t_arg) to an array of strings
-char	**arg_to_array_converter(t_arg *arg , char *command)
+// convert linked_lst of arg(t_arg) to an array of strings
+char	**arg_to_array_converter(t_arg *arg, char *command)
 {
-	int	i;
+	int		i;
 	char	**argv;
-	int	arg_counter = 1;//starting from 1 as a first command
-	t_arg	*cur_arg = arg;
-	
+	t_arg	*cur_arg;
+
+	int arg_counter = 1; // starting from 1 as a first command
+	cur_arg = arg;
 	while (cur_arg)
 	{
 		arg_counter++;
 		cur_arg = cur_arg->next;
 	}
-	argv =(char **)malloc(sizeof(char *) * (arg_counter + 1));
+	argv = (char **)malloc(sizeof(char *) * (arg_counter + 1));
 	if (!argv)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	argv[0] = command;
 	cur_arg = arg;
@@ -40,18 +42,18 @@ char	**arg_to_array_converter(t_arg *arg , char *command)
 		argv[i++] = cur_arg->value;
 		cur_arg = cur_arg->next;
 	}
-	argv[i] = NULL;//end of array
-	return(argv);
+	argv[i] = NULL; // end of array
+	return (argv);
 }
 
 ////convert linked_lst of environment var (t_env) to an array of strings
 char	**env_to_array_converter(t_arg *env)
 {
-	int	i;
-	int env_var_count;
-	t_env *cur_env;
+	int		i;
+	int		env_var_count;
+	t_env	*cur_env;
 	char	**env_array;
-	
+
 	env_var_count = 0;
 	cur_env = env;
 	while (cur_env)
@@ -61,24 +63,26 @@ char	**env_to_array_converter(t_arg *env)
 	}
 	env_array = (char **)malloc(sizeof(char *) * (env_var_count + 1));
 	if (!env_array)
-		return(NULL);
+		return (NULL);
 	cur_env = env;
 	i = 0;
 	while (cur_env)
 	{
-		env_array[i] =(char *)malloc(ft_strlen(cur_env->key) + ft_strlen(cur_env->value) + 2);//allocate memory to the key=value str
+		env_array[i] = (char *)malloc(ft_strlen(cur_env->key)
+				+ ft_strlen(cur_env->value) + 2);
+			// allocate memory to the key=value str
 		if (!env_array[i])
-			return(NULL);
-		ft_strcpy(env_array[i], cur_env->key);//copy key To do
-		ft_strcat(env_array[i], "=");//To do
+			return (NULL);
+		ft_strcpy(env_array[i], cur_env->key); // copy key To do
+		ft_strcat(env_array[i], "=");          // To do
 		ft_strcat(env_array[i], cur_env->value);
-		cur_env = cur_env->next;//move to next var in the list
+		cur_env = cur_env->next; // move to next var in the list
 		i++;
 	}
-	env_array[i] = NULL;//end of array
-	return(env_array);
+	env_array[i] = NULL; // end of array
+	return (env_array);
 }
-//Locate the full path in which a command is stored in directories listed in PATH env-var
+// Locate the full path in which a command is stored in directories listed in PATH env-var
 char	*command_fullpath_finder(char *command, t_env **env)
 {
 	
--- execute/executor_libft.c
+++ execute/executor_libft.c
@@ -6,7 +6,7 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 20:17:41 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/08/25 20:38:07 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/08/27 13:47:54 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,34 @@
 
 char	*ft_strcpy(char *dest, const char *src)
 {
-	int i = 0;
+	int	i;
+
+	i = 0;
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	return(dest);
+	return (dest);
 }
 
-char *ft_strcat(char *dest, const char *src)
+char	*ft_strcat(char *dest, const char *src)
 {
-   // Find the end of the destination string
-    char *dest_end;
+	char	*dest_end;
 
-    dest_end = dest;
-    while (*dest_end != '\0')
+	// Find the end of the destination string
+	dest_end = dest;
+	while (*dest_end != '\0')
 	{
-        dest_end++;
-    }
-    while (*src != '\0')
+		dest_end++;
+	}
+	while (*src != '\0')
 	{
 		*dest_end = *src;
-        dest_end++;
-        src++;
-    }
-
-    *dest_end = '\0';
-
-    return(dest);
+		dest_end++;
+		src++;
+	}
+	*dest_end = '\0';
+	return (dest);
 }
-
