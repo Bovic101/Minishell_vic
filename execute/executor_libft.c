0cb1dc25121408a6ce5f61552c0bf690c30b6c76
vodebunm <vodebunm@student.42heilbronn.de>
2024-09-02 14:20:31 +0200

    excutor

--- execute/executor_libft.c
+++ execute/executor_libft.c
@@ -6,7 +6,7 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 20:17:41 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/09/02 02:33:41 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/09/02 13:47:40 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,36 +46,34 @@ char	*ft_strcat(char *dest, const char *src)
 	return (dest);
 }
 
-char *str_tokenizer(char *str, const char *delim)
+char	*str_tokenizer(char *str, const char *delim)
 {
-    static char *next_token;
-    char *start;
-    char *end;
+	static char	*next_token;
+	char		*start;
+	char		*end;
+
 	next_token = NULL;
-
-    if (str != NULL)
-        next_token = str;
-    if (next_token == NULL)
-        return NULL;
-    start = next_token;
-
-    while (*start != '\0' && strchr(delim, *start) != NULL)
-        start++;
-    if (*start == '\0')
+	if (str != NULL)
+		next_token = str;
+	if (next_token == NULL)
+		return (NULL);
+	start = next_token;
+	while (*start != '\0' && strchr(delim, *start) != NULL)
+		start++;
+	if (*start == '\0')
 	{
-        next_token = NULL;
-        return NULL;
-    }
-    end = start;
-
-    while (*end != '\0' && strchr(delim, *end) == NULL)
-        end++;
-    if (*end == '\0')
+		next_token = NULL;
+		return (NULL);
+	}
+	end = start;
+	while (*end != '\0' && strchr(delim, *end) == NULL)
+		end++;
+	if (*end == '\0')
 	{
-        next_token = NULL;
-    }
+		next_token = NULL;
+	}
 	else
-        *end = '\0';
-        next_token = end + 1;
-    return start;
+		*end = '\0';
+	next_token = end + 1;
+	return (start);
 }
