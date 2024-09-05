aa2453da3c6a764d07bb4dc9118ac6b0048becc1
Katsiaryna Dvarakouskaya <kdvarako@1-C-1.42heilbronn.de>
2024-09-05 15:21:15 +0200

    added words_join.c in lext_parc

---
+++ lex_pars/words_join.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   words_join.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 15:17:11 by kdvarako          #+#    #+#             */
+/*   Updated: 2024/09/05 15:17:19 by kdvarako         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+char	*ft_wordjoin(char *value, char *str)
+{
+	int		i;
+	int		j;
+	char	*new;
+
+	if (!value || !str)
+		return (NULL);
+	new = malloc(ft_strlen(value) + ft_strlen(str) + 1);
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (value[i] != '\0')
+	{
+		new[i] = value[i];
+		i++;
+	}
+	j = 0;
+	while (str[j] != '\0')
+	{
+		new[i] = str[j];
+		i++;
+		j++;
+	}
+	new[i] = '\0';
+	free(value);
+	return (new);
+}
+
+char	*save_word(t_token **pnode, t_env **env)
+{
+	char	*str;
+	char	*value;
+	t_token	*node;
+
+	node = *pnode;
+	str = NULL;
+	value = NULL;
+	value = malloc(1);
+	if (!value)
+		return (NULL);
+	value[0] = '\0';
+	while (node != NULL && node->type != SEPARATOR)
+	{
+		if (node->type == WORD)
+			str = ft_strndup(node->ptr, node->len);
+		else if (node->type == EXPAND_VAR || node->type == EXPAND_VAR_Q)
+			str = expand(node->ptr, node->len, env, node->type);
+		value = ft_wordjoin(value, str);
+		free (str);
+		*pnode = node;
+		node = node->next;
+	}
+	return (value);
+}
