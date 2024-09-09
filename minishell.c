d964663f5b81d21fce7a8066cb33c1f4d3a4195d
vodebunm <vodebunm@student.42heilbronn.de>
2024-09-09 15:29:27 +0200

    basic formattinga and minor changes to my_prompt.c and mininisheel.c

--- minishell.c
+++ minishell.c
@@ -6,68 +6,65 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 12:26:11 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/09/09 08:56:03 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/09/09 15:25:22 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void cmd_processing(char *s, t_env **env)
+void	cmd_processing(char *s, t_env **env)
 {
-    t_token *token = NULL;
-    t_parc *parc = NULL;
+	t_token	*token;
+	t_parc	*parc;
 
-    lexer(&token, s);
-    parcer(&token, &parc, env);
-    //execute(&parc, env);
-    executor_func(parc, env);
-    freeall(&token, &parc);
+	token = NULL;
+	parc = NULL;
+	lexer(&token, s);
+	parcer(&token, &parc, env);
+	// execute(&parc, env);
+	executor_func(parc, env);
+	freeall(&token, &parc);
 }
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-    char *s;
-    t_env *env;
-    t_history *history;
-    int loop_condition = 0;
+	char		*s;
+	t_env		*env;
+	t_history	*history;
+	int			loop_condition;
 
-    (void)argc;
-    (void)argv;
-
-    env = NULL;
-    history = NULL;
-
-    save_environment(envp, &env);
-    signal(SIGINT, sigint_handler); // Signal call for Ctrl+C
-    signal(SIGQUIT, sigquit_handler);
-
-    while (!loop_condition)
-    {
-        s = readline("Our_shell:~$ ");
-        if (s == NULL) // Handle Ctrl+D
-        {
-            loop_condition = 1;
-        }
-        else if (*s == '\0') // Redisplay cmd when enter is pressed
-        {
-            free(s);
-        }
-        else if (ft_strcmp(s, "exit") == 0) // Exit the shell if the user types "exit"
-        {
-            free(s);
-            loop_condition = 1;
-        }
-        else
-        {
-            save_history(s, &history);
-            cmd_processing(s, &env);
-            free(s);
-        }
-    }
-    
-    ft_free_env(&env);
-    ft_free_history(&history);
-    return 0;
+	loop_condition = 0;
+	(void)argc;
+	(void)argv;
+	env = NULL;
+	history = NULL;
+	save_environment(envp, &env);
+	signal(SIGINT, sigint_handler); // Signal call for Ctrl+C
+	signal(SIGQUIT, sigquit_handler);
+	while (!loop_condition)
+	{
+		s = readline("Our_shell:~$ ");
+		if (s == NULL) // Handle Ctrl+D
+		{
+			loop_condition = 1;
+		}
+		else if (*s == '\0') // Redisplay cmd when enter is pressed
+		{
+			free(s);
+		}
+		else if (ft_strcmp(s, "exit") == 0)
+		{
+			free(s);
+			loop_condition = 1;
+		}
+		else
+		{
+			save_history(s, &history);
+			cmd_processing(s, &env);
+			free(s);
+		}
+	}
+	ft_free_env(&env);
+	ft_free_history(&history);
+	return (0);
 }
-
-
--- signals/my_prompt.c
+++ signals/my_prompt.c
@@ -6,11 +6,12 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 12:56:13 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/09/09 07:13:17 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/09/09 15:26:28 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
 /*
 void	prompt(void)
 {
@@ -18,11 +19,11 @@ void	prompt(void)
 }
 **/
 
-void sigint_handler(int signal)
+void	sigint_handler(int signal)
 {
 	(void)signal;
-	printf("\n"); // Ctrl+C is pressed, print newline
-    printf("Our_shell:~$ "); // Re-display the prompt
+	printf("\n");
+	printf("Our_shell:~$ "); // Re-display the prompt
 }
 
 void	sigquit_handler(int signal)
