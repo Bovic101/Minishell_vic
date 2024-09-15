d3b13e71fbb6ed8ddc3cc6c436cdf4c28615a0c9
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-09-15 13:31:25 +0200

    modif header

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 12:18:18 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/09/15 12:45:10 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/09/15 13:30:51 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -154,7 +154,7 @@ void		ft_history_print(t_history **history);
 //execution part:
 int			main_pipe_proc(t_parc **parc, t_env **env);
 //void		execute(t_parc **parc, t_env **env);
-char		**arg_to_array_converter(t_arg *arg , char *command);
+char		**arg_to_array_converter(t_arg *arg, char *command);
 char		**env_to_array_converter(t_env *env);
 char		*ft_strcpy(char *dest, const char *src);
 char		*ft_strcat(char *dest, const char *src);
