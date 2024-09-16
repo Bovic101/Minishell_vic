d24a744ca1270f8e0ac6ba0c692e41447365612c
Katsiaryna <katyamulya@gmail.com>
2024-09-16 19:40:21 +0200

    changes in cd

--- execute/execute.c
+++ execute/execute.c
@@ -78,8 +78,13 @@ void	exe_cd(t_parc *node, t_env **env)
 		res = chdir(node->args->value);
 	if (res == 0)
 	{
-		set_value("PWD", getcwd(buf, sizeof(buf)), env);
+		//printf("pwd buf: %s\n", getcwd(buf, sizeof(buf)));
+		//printf("pwd before: %s\n", get_value("PWD", env));
+		set_value("PWD", getcwd(buf, sizeof(buf)), env);  //allocate? what return getcwd - err?
+		//printf("pwd after: %s\n", get_value("PWD", env));
+		//printf("old after: %s\n", get_value("OLDPWD", env));
 		set_value("OLDPWD", cur_pwd, env);
+		//printf("old after: %s\n", get_value("OLDPWD", env));
 
 	}
 }
