cdd946fef48d8131cb70f59d26531178beb77fa2
Katsiaryna <katyamulya@gmail.com>
2024-09-19 21:29:05 +0200

    added check var name for export

--- builtins/export.c
+++ builtins/export.c
@@ -12,6 +12,27 @@
 
 #include "../minishell.h"
 
+int	right_varname_e(char *str)
+{
+	int	i;
+
+	i = 1;
+
+	if (str[0] == '_' || (str[0] >= 'a' && str[0] <= 'z') || (str[0] >= 'A' && str[0] <= 'Z'))
+	{
+		while (str[i] != '\0' && str[i] != '=')
+		{
+			if (!(str[i] == '_' || (str[i] >= 'a' && str[i] <= 'z') || \
+				(str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 0 && str[i] <= 9)))
+				return (1);
+			i++;
+		}
+	}
+	else
+		return (1);
+	return (0);
+}
+
 void	exe_export(t_parc *node, t_env **env)
 {
 	t_arg	*arg;
@@ -29,19 +50,27 @@ void	exe_export(t_parc *node, t_env **env)
 		{
 			len = 0;
 			fl = 0;
-			while (arg->value[len] != '\0')
+			if (right_varname_e(arg->value) == 0)
 			{
-				if (arg->value[len] == '=')
+				while (arg->value[len] != '\0')
 				{
-					fl = 1;
-					break ;
+					if (arg->value[len] == '=')
+					{
+						fl = 1;
+						break ;
+					}
+					len++;
 				}
-				len++;
+				if (fl == 1)
+					add_set_env(ft_strndup(arg->value, len), ft_strdup(&arg->value[len + 1]), env);
+				else
+					add_set_env(ft_strndup(arg->value, len), NULL, env);
 			}
-			if (fl == 1)
-				add_set_env(ft_strndup(arg->value, len), ft_strdup(&arg->value[len + 1]), env);
 			else
-				add_set_env(ft_strndup(arg->value, len), NULL, env);
+			{
+				printf("export: `%s': not a valid identifier\n", arg->value);
+				//err handler ???
+			}
 			arg = arg->next;
 		}
 		//ft_env_sort_declare(*env);
