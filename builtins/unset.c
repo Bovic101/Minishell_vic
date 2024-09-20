0c848626979493f25cdad017d92d3332d7bd38db
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-09-20 11:49:50 +0200

    modified unset.c

--- builtins/unset.c
+++ builtins/unset.c
@@ -6,24 +6,54 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 15:43:58 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/09/18 15:46:45 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/09/20 11:48:24 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+int	right_varname_u(char *str)
+{
+	int	i;
+
+	i = 1;
+
+	if (str[0] == '_' || (str[0] >= 'a' && str[0] <= 'z') \
+		|| (str[0] >= 'A' && str[0] <= 'Z'))
+	{
+		while (str[i] != '\0')
+		{
+			if (!(str[i] == '_' || (str[i] >= 'a' && str[i] <= 'z') || \
+				(str[i] >= 'A' && str[i] <= 'Z') \
+					|| (str[i] >= 0 && str[i] <= 9)))
+				return (1);
+			i++;
+		}
+	}
+	else
+		return (1);
+	return (0);
+}
+
 void	exe_unset(t_parc *node, t_env **env)
 {
 	t_arg	*arg;
-	/*if (node->args == NULL && node->redirs_in == NULL \
-		&& node->redirs_out == NULL)
-		*/
+
 	if (node->args != NULL)
 	{
 		arg = node->args;
 		while (arg != NULL)
 		{
-			remove_node(arg->value, env);
+			if (right_varname_u(arg->value) == 0)
+			{
+				remove_node(arg->value, env);
+			}
+			else
+			{
+				perror("unset not a valid identifier\n");
+				//printf("export: `%s': not a valid identifier\n", arg->value);
+				//err handler
+			}
 			arg = arg->next;
 		}
 	}
