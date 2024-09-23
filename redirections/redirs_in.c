aeb7ca8ac3ae93f41ea4425ee22cf796176beb5d
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-09-23 17:06:43 +0200

    modif redirs_in.c

--- redirections/redirs_in.c
+++ redirections/redirs_in.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirs_in.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 16:30:32 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/09/20 22:17:17 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/09/23 17:01:44 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ char	*ft_hdoc(char *s_end)
 				if (ft_strlen(value) != 0)
 					value = ft_wordjoin(value, "\n");
 				value = ft_wordjoin(value, s);
+				//value = ft_wordjoin(value, "\n");
 				free(s);
 			}
 			else
@@ -68,15 +69,28 @@ char	*save_last_hdoc(t_parc *node)
 	return (value);
 }
 
-int redir_in(t_parc *node)
+int redir_in_hdoc(char *hdoc)
+{
+	int	fd_h[2];
+	int	err;
+
+	err = pipe(fd_h);
+	if (err < 0)
+		return (-1);  //err handling
+	if (write(fd_h[1], hdoc, sizeof(hdoc)) < 0)
+			return (-1);  //err handling
+	dup2(fd_h[0], 0);
+	close(fd_h[0]);
+	close(fd_h[1]);
+	return (0);
+}
+
+int redir_in_file(t_parc *node)
 {
 	t_redirect  *r_in;
 	int         fd_file;
 	int         err_close;
-	//char		*hdoc;
 
-	//hdoc = save_last_hdoc(node);
-	//printf("%s", hdoc);
 	r_in = node->redirs_in;
 	while (r_in)
 	{
@@ -93,12 +107,34 @@ int redir_in(t_parc *node)
 			err_close = close(fd_file);
 			if (err_close == -1)
 			{
-				printf("Error to close file\n");
+				printf("Error to close\n");
 				return (-1); //err handling
 			}
 		}
 		r_in = r_in->next;
 	}
-	//free hdoc if not NULL
+	return (0);
+}
+
+
+int	redir_in(t_parc *node)
+{
+	char		*hdoc;
+	t_redirect	*last;
+
+	hdoc = save_last_hdoc(node);
+	//printf("%s", hdoc);
+	//printf("\n");
+	last = ft_redir_last(node->redirs_in);
+	//printf("last type: %s\n", last->rtype);
+	if (ft_strcmp(last->rtype, "<<") == 0)
+	{
+		redir_in_hdoc(hdoc);
+		free(hdoc);
+	}
+	else
+		free(hdoc);
+	if (redir_in_file(node) == -1)
+		return (-1);
 	return (0);
 }
