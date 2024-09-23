b9e18d5538fed132c7c36d60b00b4796b6493342
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-09-23 17:06:05 +0200

    modif ft_redirections.c

--- redirections/ft_redirections.c
+++ redirections/ft_redirections.c
@@ -6,7 +6,7 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 16:33:30 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/09/20 16:33:34 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/09/23 15:56:55 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,20 @@
 int	ft_redirections(t_parc **parc)
 {
 	t_parc	*node;
+	int		ret_inr;
 
 	//<< && <
 	node = *parc;
+	ret_inr = 0;
 	while (node)
 	{
 		if (node->redirs_in != NULL)
 		{
-			redir_in(node);
+			ret_inr = redir_in(node);
 		}
 		node = node->next;
 	}
+	//printf("ret_inr = %d\n", ret_inr);
 	//>> && >
 	node = *parc;
 	while (node)
@@ -36,5 +39,7 @@ int	ft_redirections(t_parc **parc)
 		}
 		node = node->next;
 	}
+	if (ret_inr == -1)
+		return (-1);
 	return (0);
 }
