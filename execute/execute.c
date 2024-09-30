8b779cd34fe0886c06c8c09c726ec9f86f9ca623
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-09-30 12:37:50 +0200

    added return to builtins

--- execute/execute.c
+++ execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 11:29:41 by kdvarako          #+#    #+#             */
-/*   Updated: 2024/09/20 13:02:01 by kdvarako         ###   ########.fr       */
+/*   Updated: 2024/09/30 12:36:35 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,21 +36,26 @@ int	if_builtin(char *cmd)
 
 int	execute_builtin(t_parc *node, t_env **env)
 {
+	int	return_status;
+
 	if (ft_strcmp(node->cmd, "echo") == 0)
-		exe_echo(node);
+		return_status = exe_echo(node);
 	else if (ft_strcmp(node->cmd, "cd") == 0)
-		exe_cd(node, env);
+		return_status = exe_cd(node, env);
 	else if (ft_strcmp(node->cmd, "pwd") == 0)
-		exe_pwd(env);
+		return_status = exe_pwd(env);
 	else if (ft_strcmp(node->cmd, "export") == 0)
-		exe_export(node, env);
+		return_status = exe_export(node, env);
 	else if (ft_strcmp(node->cmd, "unset") == 0)
-		exe_unset(node, env);
+		return_status = exe_unset(node, env);
 	else if (ft_strcmp(node->cmd, "env") == 0)
-		exe_env(env);
+		return_status = exe_env(env);
 	//else if (ft_strcmp(node->cmd, "exit") == 0)
 		//exe_exit(node, env);
+	if (return_status == 0)
 		return (0);
+	else
+		return (-1);
 }
 
 /*
