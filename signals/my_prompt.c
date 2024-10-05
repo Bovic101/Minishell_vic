8c5dcb62fa88810f5a14f15a725a1298e05fa948
Katsiaryna Dvarakouskaya <kdvarako@2-F-6.42heilbronn.de>
2024-10-05 15:55:26 +0200

    added void in ()

--- signals/my_prompt.c
+++ signals/my_prompt.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   my_prompt.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 12:56:13 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/10/05 11:16:44 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/10/05 15:53:46 by kdvarako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ void	sigint_handler(int signal)
 	(void)signal;
 	write(STDOUT_FILENO, "\n",1);
 	rl_on_new_line();//func inform readline that user pressed ctrl +c
-    rl_point= 0;//manually clearing the input
-    rl_end=0;
+	rl_point= 0;//manually clearing the input
+	rl_end=0;
 	rl_redisplay();//func redsisplay the prompt
 }
 
@@ -37,18 +37,18 @@ void	sigquit_handler(int signal)
 
 int eof_handler(int count, int key) 
 {
-    (void)key;
-    if (count == 0)  // If the line is empty (count is 0 when hit Ctrl-D 
+	(void)key;
+	if (count == 0)  // If the line is empty (count is 0 when hit Ctrl-D 
 	{
-        printf("\nexit\n");
-        exit(0);
-        return 0; // Return a value
-    }
-    return 0;
+		printf("\nexit\n");
+		exit(0);
+		return 0; // Return a value
+	}
+	return 0;
 }
 
 // Set up signal handlers
-void signal_handlers_caller()
+void	signal_handlers_caller(void)
 {
 	struct sigaction sa_int;
 	struct sigaction sa_quit;
@@ -57,12 +57,12 @@ void signal_handlers_caller()
 	ft_memset(&sa_quit, 0, sizeof(sa_quit));
 
 	sigemptyset(&sa_int.sa_mask);  // Set up SIGQUIT handler (Ctrl + c)
-    sa_int.sa_handler = sigint_handler;
-    sa_int.sa_flags = 0;
-    sigaction(SIGINT, &sa_int, NULL);
+	sa_int.sa_handler = sigint_handler;
+	sa_int.sa_flags = 0;
+	sigaction(SIGINT, &sa_int, NULL);
 
-    sigemptyset(&sa_quit.sa_mask); // Set up SIGQUIT handler (Ctrl-\)
-    sa_quit.sa_handler = sigquit_handler;
-    sa_quit.sa_flags = 0;
-    sigaction(SIGQUIT, &sa_quit, NULL);
+	sigemptyset(&sa_quit.sa_mask); // Set up SIGQUIT handler (Ctrl-\)
+	sa_quit.sa_handler = sigquit_handler;
+	sa_quit.sa_flags = 0;
+	sigaction(SIGQUIT, &sa_quit, NULL);
 }
