a220c75d15884a76db8ba0d40b7ce3d6f74a618e
Katsiaryna Dvarakouskaya <kdvarako@1-A-29.42heilbronn.de>
2024-10-06 17:48:30 +0200

    added strjoin with free

---
+++ lex_pars/ft_strjoin_free.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin_free.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdvarako <kdvarako@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/06 17:21:01 by kdvarako          #+#    #+#             */
+/*   Updated: 2024/10/06 17:23:07 by kdvarako         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+char	*ft_strjoin_free(char *s1, char *s2)
+{
+	char	*s;
+	int		i;
+	int		j;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	s = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!s)
+		return (free(s1), free(s2), NULL);
+	while (s1[i] != '\0')
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		s[i] = s2 [j];
+		i++;
+		j++;
+	}
+	s[i] = '\0';
+	return (free(s1), free(s2), s);
+}
